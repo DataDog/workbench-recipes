8b35b0af465a7acc214a323673c332004f889dad
eplanet <emeric.planet@datadoghq.com>
2017-06-22 18:26:11 +0200

    Move sample data to generic directory

--- mongodb/provision/replica_set.sh
+++ mongodb/provision/replica_set.sh
@@ -6,5 +6,5 @@ mongo mongo-rep1:27017 --eval 'rs.initiate()'
 mongo mongo-rep1:27017 --eval 'rs.add("mongo-rep2")'
 mongo mongo-rep1:27017 --eval 'rs.add("mongo-rep3")'
 
-mongoimport -h mongo-rep1 --db dummy --collection restaurants --file /provision/restaurants.json
-mongoimport -h mongo-rep1 --db dummy --collection employees --file /provision/employees.json
+mongoimport -h mongo-rep1 --db dummy --collection restaurants --file /_assets/json/restaurants.json
+mongoimport -h mongo-rep1 --db dummy --collection employees --file /_assets/json/employees.json
